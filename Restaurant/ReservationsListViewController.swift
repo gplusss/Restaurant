d140b341fe7896b11410539ad954afb1c905757c
Vladimir Saprykin <gplusss@gmail.com>
2016-04-15 08:35:17 +0300

    Did Delete Need Sort

--- Restaurant/ReservationsListViewController.swift
+++ Restaurant/ReservationsListViewController.swift
@@ -8,16 +8,16 @@
 
 import Foundation
 import UIKit
+import RealmSwift
 
 class ReservationsListViewController: UITableViewController {
     var table: Table!
         var cellArray : [Reservation] = []
     
-    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(true)
-//        table.reservations.sortInPlace ({ $0.startTime.compare($1.endTime) == NSComparisonResult.OrderedAscending })
-
+        //table.reservations.sortInPlace ({ $0.startTime.compare($1.endTime) == NSComparisonResult.OrderedAscending })
+        table.reservations.sorted("startTime", ascending: true)
         tableView.reloadData()
     }
     
@@ -37,6 +37,9 @@ class ReservationsListViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ReservationTableViewCell", forIndexPath: indexPath) as! ReservationTableViewCell
         
+        let realm = try! Realm()
+        realm.beginWrite()
+        
         let reservation = table.reservations[indexPath.row]
         
         let dateFormatter = NSDateFormatter()
@@ -47,22 +50,29 @@ class ReservationsListViewController: UITableViewController {
         cell.startTimeLabel.text = "From: \(dateFormatter.stringFromDate(reservation.startTime))"
         cell.endTimeLabel.text = "To: \(dateFormatter.stringFromDate(reservation.endTime))"
         cell.phoneNumberLabel.text = "Phone +38 \(reservation.phone)"
+        
+        try! realm.commitWrite()
         return cell
     }
     
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-
-    }
+//    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableView, forRowAtIndexPath indexPath: UITableView) {
+//
+//    }
     override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
         let deleteAction = UITableViewRowAction(style: .Default, title: "Delete") { action -> Void in
+            let realm = try! Realm()
+            realm.beginWrite()
+            
             self.table.reservations.removeAtIndex(indexPath.row)
+            
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        //let item = self.table.reservations[indexPath.row]
-        //let activityViewController = UIActivityViewController(activityItems: [item], applicationActivities: nil)
-        //self.presentViewController(activityViewController, animated: true, completion: nil)
+            try! realm.commitWrite()
+            
     }
         deleteAction.backgroundColor = UIColor.blueColor()
+        
         return [deleteAction]
+        
     }
     
 
